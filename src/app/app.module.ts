3d6f468269815e833de1da68126d0559c4e10332
Manish Bisht <manish.bisht490@gmail.com>
2021-01-27 21:30:33 +0530

    project complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {Router, RouterModule,Routes} from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { LangComponent } from './pages/lang/lang.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ContactComponent } from './pages/contact/contact.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './pages/home/home.component';
+import {AboutComponent} from './pages/about/about.component';
+import {LangComponent} from './pages/lang/lang.component';
+import {ProjectsComponent} from './pages/projects/projects.component';
+import {ContactComponent} from './pages/contact/contact.component';
 
-const AppRoutes:Routes=[
-  {path:"",component:HomeComponent},
-  {path:"about",component:AboutComponent},
-  {path:"lang",component:LangComponent},
-  {path:"projects",component:ProjectsComponent},
-  {path:"contactme",component:ContactComponent}
-]
 @NgModule({
-
-
- 
   declarations: [
     AppComponent,
     HomeComponent,
@@ -26,12 +15,12 @@ const AppRoutes:Routes=[
     LangComponent,
     ProjectsComponent,
     ContactComponent,
-    
   ],
   imports: [
-    BrowserModule,RouterModule.forRoot(AppRoutes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
